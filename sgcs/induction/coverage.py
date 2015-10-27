de4ebecc50647eadc1f42442508ec22f9628fc84
MichalStempkowski <michulix@gmail.com>
2015-10-27 17:45:39 +0100

    Universal coverage tub

--- sgcs/induction/coverage.py
+++ sgcs/induction/coverage.py
@@ -1,15 +1,46 @@
+from abc import abstractmethod
 from sgcs.induction.rule import TerminalRule
-from sgcs.induction.symbol import Symbol
 
 
-class TerminalCoverageOperator(object):
-    def __init__(self, cyk_service):
+class CoverageOperator(object):
+    def __init__(self, cyk_service, chance):
         self.cyk_service = cyk_service
+        self.chance = chance
 
     def cover(self, environment, rule_population, coordinates):
-        chance = self.cyk_service.configuration.coverage.operators.terminal.chance
-        if self.cyk_service.randomizer.perform_with_chance(chance):
-            parent = rule_population.get_random_terminal_symbol(self.cyk_service.randomizer)
-            return TerminalRule(parent, environment.get_sentence_symbol(coordinates[1]))
+        if self.cyk_service.randomizer.perform_with_chance(self.chance):
+            return self.cover_impl(environment, rule_population, coordinates)
 
         return None
+
+    @abstractmethod
+    def cover_impl(self, environment, rule_population, coordinates):
+        pass
+
+
+class TerminalCoverageOperator(CoverageOperator):
+    def __init__(self, cyk_service):
+        super().__init__(cyk_service,
+                         cyk_service.configuration.coverage.operators.terminal.chance)
+
+    def cover_impl(self, environment, rule_population, coordinates):
+        parent = rule_population.get_random_terminal_symbol(self.cyk_service.randomizer)
+        return TerminalRule(parent, environment.get_sentence_symbol(coordinates[1]))
+
+
+class UniversalCoverageOperator(CoverageOperator):
+    def __init__(self, cyk_service):
+        super().__init__(cyk_service,
+                         cyk_service.configuration.coverage.operators.terminal.chance)
+
+    def cover_impl(self, environment, rule_population, coordinates):
+        return True
+
+
+class StartingCoverageOperator(CoverageOperator):
+    def __init__(self, cyk_service):
+        super().__init__(cyk_service,
+                         cyk_service.configuration.coverage.operators.terminal.chance)
+
+    def cover_impl(self, environment, rule_population, coordinates):
+        return True
--- sgcs/tests/induction/test_coverage.py
+++ sgcs/tests/induction/test_coverage.py
@@ -2,7 +2,8 @@ from enum import Enum
 import unittest
 from unittest.mock import MagicMock, create_autospec
 from hamcrest import *
-from sgcs.induction.coverage import TerminalCoverageOperator
+from sgcs.induction.coverage import TerminalCoverageOperator, UniversalCoverageOperator, \
+    StartingCoverageOperator
 from sgcs.induction.cyk_configuration import CykConfiguration, CoverageConfiguration, \
     CoverageOperators
 from sgcs.induction.cyk_service import CykService
@@ -19,7 +20,7 @@ class CykReasoningStatus(Enum):
     no_starting_symbol = 2
 
 
-class TestTerminalCoverageOperator(unittest.TestCase):
+class CoverageOperatorTestCommon(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -30,17 +31,15 @@ class TestTerminalCoverageOperator(unittest.TestCase):
         self.cyk_service_mock.randomizer = create_autospec(Randomizer)
         self.cyk_service_mock.configuration.return_value = self.cyk_configuration_mock
 
-        self.sut = TerminalCoverageOperator(self.cyk_service_mock)
-
         self.environment_mock = create_autospec(Environment)
-        self.environment_mock.get_sentence_symbol.return_value = Symbol(hash('a'))
 
         self.rule_population_mock = create_autospec(RulePopulation)
-        self.rule_population_mock.get_random_terminal_symbol.return_value = Symbol(hash('A'))
 
         self.coordinates = 0, 2
 
-    def test_operators_execute_with_some_chance(self):
+        self.sut = None
+
+    def operator_executes_with_some_chance_scenario(self):
         self.cyk_service_mock.randomizer.perform_with_chance.side_effect = [False, True]
         assert_that(
             self.sut.cover(self.environment_mock, self.rule_population_mock, self.coordinates),
@@ -50,8 +49,51 @@ class TestTerminalCoverageOperator(unittest.TestCase):
             self.sut.cover(self.environment_mock, self.rule_population_mock, self.coordinates),
             is_not(None))
 
-    def test_given_unknown_terminal_symbol_should_announce_terminal_rule_creation(self):
+
+class TestTerminalCoverageOperator(CoverageOperatorTestCommon):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.sut = TerminalCoverageOperator(self.cyk_service_mock)
+
+        self.environment_mock.get_sentence_symbol.return_value = Symbol(hash('a'))
+        self.rule_population_mock.get_random_terminal_symbol.return_value = Symbol(hash('A'))
+
+    def test_operators_execute_with_some_chance(self):
+        self.operator_executes_with_some_chance_scenario()
+
+    def test_given_unknown_terminal_symbol_should_cover_it(self):
         result = self.sut.cover(self.environment_mock, self.rule_population_mock, self.coordinates)
         assert_that(
             result,
             is_(equal_to(TerminalRule(Symbol(hash('A')), Symbol(hash('a'))))))
+
+
+class TestUniversalCoverageOperator(CoverageOperatorTestCommon):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.sut = UniversalCoverageOperator(self.cyk_service_mock)
+
+        # self.environment_mock.get_sentence_symbol.return_value = Symbol(hash('a'))
+        # self.rule_population_mock.get_random_terminal_symbol.return_value = Symbol(hash('A'))
+
+    def test_operators_execute_with_some_chance(self):
+        self.operator_executes_with_some_chance_scenario()
+
+    # def test_given_unknown_terminal_symbol_should_cover_it(self):
+
+
+class TestStartingCoverageOperator(CoverageOperatorTestCommon):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.sut = StartingCoverageOperator(self.cyk_service_mock)
+
+        # self.environment_mock.get_sentence_symbol.return_value = Symbol(hash('a'))
+        # self.rule_population_mock.get_random_terminal_symbol.return_value = Symbol(hash('A'))
+
+    def test_operators_execute_with_some_chance(self):
+        self.operator_executes_with_some_chance_scenario()
+
+    # def test_given_sentence_of_length_1_should_cover_it(self):
