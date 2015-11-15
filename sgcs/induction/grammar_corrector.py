c098d57368ada429e5db826666d1fe6c67c79485
MichalStempkowski <michulix@gmail.com>
2015-11-15 09:57:31 +0100

    GrammarCorrector::remove_not_reachable implemented

--- sgcs/induction/grammar_corrector.py
+++ sgcs/induction/grammar_corrector.py
@@ -1,25 +1,46 @@
 class GrammarCorrector(object):
     def correct_grammar(self, rule_population, cyk_service):
         self.remove_non_productive(rule_population, cyk_service)
+        self.remove_not_reachable(rule_population, cyk_service)
 
-    def remove_non_productive(self, rule_population, cyk_service):
-        productive_rules = set()
-        last_productive_rules = set()
+    @staticmethod
+    def _rule_remover(rule_population, cyk_service, starting_rules, select_predicate,
+                      symbol_adder):
+        selected_rules = set()
+        selected_rules.update(starting_rules)
 
-        terminal_rules = list(rule_population.get_terminal_rules())
-        symbols = set(map(lambda x: x.parent, terminal_rules))
-        productive_rules.update(terminal_rules)
-
-        while len(productive_rules) != len(last_productive_rules):
-            last_productive_rules = productive_rules.copy()
+        while True:
+            last_selected_rules = selected_rules.copy()
             for rule in rule_population.get_all_non_terminal_rules():
-                if rule.left_child in symbols and rule.right_child in symbols or \
-                   rule_population.universal_symbol is not None and \
-                                        rule_population.universal_symbol in symbols:
-                    productive_rules.add(rule)
-                    symbols.add(rule.parent)
+                if select_predicate(rule):
+                    selected_rules.add(rule)
+                    symbol_adder(rule)
+
+            if len(selected_rules) == len(last_selected_rules):
+                break
 
         for rule in rule_population.get_all_non_terminal_rules():
-            if rule not in productive_rules:
+            if rule not in selected_rules:
                 rule_population.remove_rule(rule)
                 cyk_service.statistics.on_rule_removed(rule)
+
+    def remove_non_productive(self, rule_population, cyk_service):
+        starting_rules = list(rule_population.get_terminal_rules())
+        symbols = set(map(lambda x: x.parent, starting_rules))
+        select_predicate = lambda rule: \
+            rule.left_child in symbols and rule.right_child in symbols or \
+            rule_population.universal_symbol is not None and \
+            rule_population.universal_symbol in symbols
+        symbol_adder = lambda rule: symbols.add(rule.parent)
+
+        self._rule_remover(rule_population, cyk_service, starting_rules, select_predicate,
+                           symbol_adder)
+
+    def remove_not_reachable(self, rule_population, cyk_service):
+        starting_rules = list()
+        symbols = {rule_population.starting_symbol}
+        select_predicate = lambda rule: rule.parent in symbols
+        symbol_adder = lambda rule: (symbols.add(rule.left_child), symbols.add(rule.right_child))
+
+        self._rule_remover(rule_population, cyk_service, starting_rules, select_predicate,
+                           symbol_adder)
--- sgcs/tests/induction/test_grammar_corrector.py
+++ sgcs/tests/induction/test_grammar_corrector.py
@@ -17,17 +17,22 @@ class TestGrammarCorrector(unittest.TestCase):
         self.sut = GrammarCorrector()
 
         self.rule_population_mock = create_autospec(RulePopulation)
-        self.rule_population_mock.configure_mock(universal_symbol=Symbol('U'))
+        self.rule_population_mock.configure_mock(universal_symbol=Symbol('U'),
+                                                 starting_symbol=Symbol('S'))
         self.cyk_service_mock = create_autospec(CykService)
         self.cyk_service_mock.configure_mock(statistics=create_autospec(CykStatistics))
 
         self.rule_a = self.mk_rule('A', 'a')
         self.rule_b = self.mk_rule('B', 'a')
         self.universal_symbol = self.mk_rule('U', 'c')
+
         self.accessible_rule = self.mk_rule('C', 'A', 'B')
         self.second_lvl_accessible_rule = self.mk_rule('T', 'A', 'B')
         self.not_accessible_rule = self.mk_rule('X', 'H', 'B')
         self.starting_symbol = self.mk_rule('S', 'H', 'H')
+
+        self.rule_accessible_from_top = self.mk_rule('H', 'J', 'G')
+        self.second_lvl_rule_accessible_from_top = self.mk_rule('G', 'M', 'C')
 
     @staticmethod
     def mk_rule(parent, left_child, right_child=None):
@@ -36,26 +41,52 @@ class TestGrammarCorrector(unittest.TestCase):
         else:
             return Rule(Symbol(parent), Symbol(left_child), Symbol(right_child))
 
-    def production_correction_scenario(self, terminal_rules, to_be_removed):
+    def production_correction_scenario(self, terminal_rules, to_be_removed, correction_function):
         self.rule_population_mock.get_terminal_rules.return_value = \
             iter(terminal_rules)
         self.rule_population_mock.get_all_non_terminal_rules.return_value = [
             self.accessible_rule,
             self.not_accessible_rule,
             self.starting_symbol,
-            self.second_lvl_accessible_rule
+            self.second_lvl_accessible_rule,
+            self.rule_accessible_from_top,
+            self.second_lvl_rule_accessible_from_top
         ]
 
-        self.sut.remove_non_productive(self.rule_population_mock, self.cyk_service_mock)
+        correction_function(self.rule_population_mock, self.cyk_service_mock)
 
-        self.rule_population_mock.remove_rule.assert_has_calls(map(call, to_be_removed))
-        assert_that(self.rule_population_mock.remove_rule.call_count,
-                    is_(equal_to(len(to_be_removed))))
+        call_args = set(map(lambda x: x[0][0],
+                            self.rule_population_mock.remove_rule.call_args_list))
+        assert_that(call_args, contains_inanyorder(*to_be_removed))
 
     def test_universal_symbol_practically_disables_non_productive_correction(self):
         self.production_correction_scenario([self.rule_a, self.rule_b, self.universal_symbol],
-                                            [])
+                                            [], self.sut.remove_non_productive)
 
     def test_should_be_able_to_remove_non_productive_rules(self):
         self.production_correction_scenario([self.rule_a, self.rule_b],
-                                            [self.not_accessible_rule, self.starting_symbol])
+                                            [self.not_accessible_rule,
+                                             self.starting_symbol,
+                                             self.rule_accessible_from_top,
+                                             self.second_lvl_rule_accessible_from_top],
+                                            self.sut.remove_non_productive)
+
+    def test_should_be_able_to_remove_not_reachable_rules(self):
+        self.production_correction_scenario([self.rule_a, self.rule_b, self.universal_symbol],
+                                            [self.not_accessible_rule,
+                                             self.second_lvl_accessible_rule],
+                                            self.sut.remove_not_reachable)
+
+    def test_should_be_able_to_remove_all_unnecessary_rules(self):
+        self.production_correction_scenario([self.rule_a, self.rule_b, self.universal_symbol],
+                                            [self.not_accessible_rule,
+                                             self.second_lvl_accessible_rule],
+                                            self.sut.correct_grammar)
+
+        self.production_correction_scenario([self.rule_a, self.rule_b],
+                                            [self.not_accessible_rule,
+                                             self.starting_symbol,
+                                             self.rule_accessible_from_top,
+                                             self.second_lvl_rule_accessible_from_top,
+                                             self.second_lvl_accessible_rule],
+                                            self.sut.correct_grammar)
