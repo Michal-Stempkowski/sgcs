b74986763508072b23ef4a64455f1834eb297d8f
MichalStempkowski <michulix@gmail.com>
2015-12-22 14:31:44 +0100

    All checkboxes now belong to us! :)

--- sgcs/gui/options_configurator.py
+++ sgcs/gui/options_configurator.py
@@ -91,7 +91,7 @@ class EvolutionAutoUpdater(AutoUpdater):
             options_configurator.configuration.evolution.operators.crossover.chance)
 
 
-class CoverageAutoUpdater(AutoUpdater):
+class InductionAutoUpdater(AutoUpdater):
     def __init__(self, options_configurator):
         super().__init__(
             lambda: self._bind(options_configurator),
@@ -102,41 +102,64 @@ class CoverageAutoUpdater(AutoUpdater):
     @staticmethod
     def _bind(options_configurator):
         options_configurator.bind_spinner(
-            options_configurator.ui.terminalProbabilityDoubleSpinBox)
+            options_configurator.ui.terminalProbabilityDoubleSpinBox
+        )
         options_configurator.bind_spinner(
-            options_configurator.ui.universalProbabilityDoubleSpinBox)
+            options_configurator.ui.universalProbabilityDoubleSpinBox
+        )
         options_configurator.bind_spinner(
-            options_configurator.ui.aggressiveProbabilityDoubleSpinBox)
+            options_configurator.ui.aggressiveProbabilityDoubleSpinBox
+        )
         options_configurator.bind_spinner(
-            options_configurator.ui.startingProbabilityDoubleSpinBox)
+            options_configurator.ui.startingProbabilityDoubleSpinBox
+        )
         options_configurator.bind_spinner(
-            options_configurator.ui.fullProbabilityDoubleSpinBox)
+            options_configurator.ui.fullProbabilityDoubleSpinBox
+        )
+        options_configurator.bind_checkbox(
+            options_configurator.ui.grammarCorrectionCheckBox
+        )
 
     @staticmethod
     def _update_model(options_configurator):
         options_configurator.configuration.induction.coverage.operators.terminal.chance = \
             options_configurator.ui.terminalProbabilityDoubleSpinBox.value()
+
         options_configurator.configuration.induction.coverage.operators.universal.chance = \
             options_configurator.ui.universalProbabilityDoubleSpinBox.value()
+
         options_configurator.configuration.induction.coverage.operators.aggressive.chance = \
             options_configurator.ui.aggressiveProbabilityDoubleSpinBox.value()
+
         options_configurator.configuration.induction.coverage.operators.starting.chance = \
             options_configurator.ui.startingProbabilityDoubleSpinBox.value()
+
         options_configurator.configuration.induction.coverage.operators.full.chance = \
             options_configurator.ui.fullProbabilityDoubleSpinBox.value()
+
+        options_configurator.configuration.induction.grammar_correction.should_run = \
+            options_configurator.ui.grammarCorrectionCheckBox.isChecked()
 
     @staticmethod
     def _update_gui(options_configurator):
         options_configurator.ui.terminalProbabilityDoubleSpinBox.setValue(
-            options_configurator.configuration.induction.coverage.operators.terminal.chance)
+            options_configurator.configuration.induction.coverage.operators.terminal.chance
+        )
         options_configurator.ui.universalProbabilityDoubleSpinBox.setValue(
-            options_configurator.configuration.induction.coverage.operators.universal.chance)
+            options_configurator.configuration.induction.coverage.operators.universal.chance
+        )
         options_configurator.ui.aggressiveProbabilityDoubleSpinBox.setValue(
-            options_configurator.configuration.induction.coverage.operators.aggressive.chance)
+            options_configurator.configuration.induction.coverage.operators.aggressive.chance
+        )
         options_configurator.ui.startingProbabilityDoubleSpinBox.setValue(
-            options_configurator.configuration.induction.coverage.operators.starting.chance)
+            options_configurator.configuration.induction.coverage.operators.starting.chance
+        )
         options_configurator.ui.fullProbabilityDoubleSpinBox.setValue(
-            options_configurator.configuration.induction.coverage.operators.full.chance)
+            options_configurator.configuration.induction.coverage.operators.full.chance
+        )
+        options_configurator.ui.grammarCorrectionCheckBox.setChecked(
+            options_configurator.configuration.induction.grammar_correction.should_run
+        )
 
 
 class CrowdingAutoUpdater(AutoUpdater):
@@ -177,15 +200,22 @@ class ElitismAutoUpdater(AutoUpdater):
 
     @staticmethod
     def _bind(options_configurator):
+        options_configurator.bind_checkbox(options_configurator.ui.isElitismUsedCheckBox)
         options_configurator.bind_spinner(options_configurator.ui.eliteSizeSpinBox)
 
     @staticmethod
     def _update_model(options_configurator):
+        options_configurator.configuration.rule.adding.elitism.is_used = \
+            options_configurator.ui.isElitismUsedCheckBox.isChecked()
+
         options_configurator.configuration.rule.adding.elitism.size = \
             options_configurator.ui.eliteSizeSpinBox.value()
 
     @staticmethod
     def _update_gui(options_configurator):
+        options_configurator.ui.isElitismUsedCheckBox.setChecked(
+            options_configurator.configuration.rule.adding.elitism.is_used)
+
         options_configurator.ui.eliteSizeSpinBox.setValue(
             options_configurator.configuration.rule.adding.elitism.size)
 
@@ -310,7 +340,7 @@ class OptionsConfigurator(GenericWidget):
                 auto_updater=EvolutionAutoUpdater(self)
             ),
             DynamicNode(
-                auto_updater=CoverageAutoUpdater(self)
+                auto_updater=InductionAutoUpdater(self)
             ),
             DynamicNode(
                 auto_updater=CrowdingAutoUpdater(self)
