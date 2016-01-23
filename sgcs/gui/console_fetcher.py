ffd4bc1796403364bf5a6a24b7bc537e4bc4478d
MichalStempkowski <michulix@gmail.com>
2016-01-23 17:42:56 +0100

    small negative fix [66] + console_fetcher without gui

--- sgcs/gui/console_fetcher.py
+++ sgcs/gui/console_fetcher.py
@@ -1,13 +1,16 @@
 import logging
+import os
+import shutil
 import sys
 from optparse import OptionParser
 
 from PyQt4 import QtGui
 
 from algorithm.task_model import TaskModel
+from executors.simulation_executor import SimulationExecutor
 from gui.non_gui_scheduler import NonGuiScheduler
 from gui.runner import Runner
-from utils import chunk
+from utils import chunk, rmdir_forced
 
 
 def main():
@@ -50,11 +53,57 @@ def main():
 
     if options.run:
         print('Starting run')
-        app = QtGui.QApplication(sys.argv)
-        scheduler = NonGuiScheduler(options.output, tasks)
-        input_data_lookup = Runner(scheduler)
-        input_data_lookup.show()
-        sys.exit(app.exec_())
+        executor = SimulationExecutor()
+        runner = None
+        for i, x in enumerate(tasks):
+            run_func, configuration, population_printer = executor.prepare_simulation(
+                runner, i, x.input_file, x.params_configuration)
+
+            result = run_func(configuration)
+
+            collected = False
+            while not collected:
+                try:
+                    _collect_task(x, result, i, configuration, population_printer, executor)
+                except PermissionError:
+                    collected = False
+                    print('not collected!')
+                else:
+                    collected = True
+
+
+def _collect_task(task, result, task_id, configuration, population_printer, executor, root_dir):
+    run_estimator, ngen, grammar_estimator, population, generalisation_data = result
+
+    path = _prepare_artifact_dir(task_id, root_dir)
+
+    input_data_name = os.path.basename(task.data_configuration)
+    config_data_name = os.path.basename(task.params_configuration)
+
+    shutil.copy(task.data_configuration, os.path.join(path, input_data_name))
+    shutil.copy(task.params_configuration, os.path.join(path, config_data_name))
+
+    executor.save_population(
+        population, population_printer, path, 'final_population'
+    )
+    executor.save_grammar_estimator(
+        grammar_estimator, path, 'grammar_estimator'
+    )
+    executor.save_execution_summary(
+        run_estimator, ngen, generalisation_data, path, 'run_summary'
+    )
+    executor.generate_grammar_estimation_diagrams(
+        grammar_estimator, path, configuration
+    )
+
+
+def _prepare_artifact_dir(task_id, root_dir):
+    path = os.path.join(root_dir, 'task_{0}'.format(task_id))
+    if os.path.exists(path):
+        rmdir_forced(path)
+
+    os.mkdir(path)
+    return path
 
 if __name__ == '__main__':
     main()
